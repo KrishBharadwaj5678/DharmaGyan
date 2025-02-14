b1b1cbc4640d2fc0c14316fd00f4981808d3e577
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2025-02-14 12:18:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This ensures the app listens on all network interfaces
+    port: process.env.PORT || 3000, // Uses the PORT environment variable or defaults to 3000
+  },
 })
